e3f02184dc3506df43f0d7b36c42e77965160fdd
Alex <alexcramer@gmx.de>
2020-01-23 22:18:52 +0100

    New cleandata2.py

--- cleandata2.py
+++ cleandata2.py
@@ -1,16 +1,16 @@
-import os
 import holidays
+import numpy as np
+import pandas as pd
 
 
-
-def cleanData(df, ):
+def cleanData(df):
     # eliminate rows where store is empty
     df = df[~df.loc[:, 'Store'].isnull()]
     df.reset_index(inplace=True)
 
-    print("eliminated rows without store-id is empty")
+    print("Dropped rows without store-ids")
 
-    #Join Function Needed here!
+#    Join Function Needed here!
 
     # extract year, month and day from Date
     date = pd.DatetimeIndex(df.loc[:, 'Date'])
@@ -18,63 +18,65 @@ def cleanData(df, ):
     df.loc[:, 'Month'] = date.month
     df.loc[:, 'Day'] = date.day
 
-    print("extract year, month and day from Date")
+    print("Extracted year, month and day from Date")
 
-    # extract day of week
+#   extract day of week
     df.loc[:, 'DayOfWeek'] = date.dayofweek + 1
 
-    print("extract day of week")
+    print("Extracted and reset day of week")
 
     for i in range(len(df)):
         if (np.isnan(df['Sales'][i])) & (df['Customers'][i] == 0):
             df['Sales'][i] = 0
         else:
             pass
-    print('set Sales to 0 if customers are 0')
+    print('Set Sales to 0 if customers are 0')
 
-    #deleting 0 sales rows
+#   deleting 0 sales rows
     df = df[df['Sales'] != 0]
+    df.reset_index(inplace=True)
     print('Dropped 0-sales rows in df')
 
-    # """Sets Open to 1 if Sales happened while Open is 0"""
+#   Sets Open to 1 if Sales happened while Open is 0
+
     for i in range(len(df)):
         if (df['Sales'][i] > 0) & (np.isnan(df['Open'][i])):
             df['Open'][i] = 1
         else:
             pass
 
-    print('Set Open = 1 if Sales > 0 in df')
+    print('Set Open = 1 if Sales > 0')
 
+#   function to fill school holiday based on state holiday
 
-    # function to fill school holiday based on state holiday
     def helper_schoolholiday(row):
         for i in range(len(df)):
-            if pd.isnull(df.row['SchoolHoliday']):
+            if pd.isnull(df['SchoolHoliday'][i]):
                 return 0.0
             else:
-                return row['SchoolHoliday']
+                return df['SchoolHoliday'][i]
 
     df['SchoolHoliday'] = df.apply(helper_schoolholiday, axis=1)
-    print("fill school holiday based o state holiday")
+    print("Filled school holidays based on state holidays")
 
-    #Taking care of shops in train stations
-    def train_station_stores_nan_open(df):
-        mask = df.loc[:, 'DayOfWeek'] == 7.0
-        dftrain2 = df[mask]
-        dftrain3 = dftrain2.groupby('Store')['Open'].sum().to_frame().rename(columns={'Open': 'newopen'})
-        train_station_stores = [i for i in dftrain3[dftrain3.newopen > 3].index]
+# #   Taking care of shops in train stations
+#     def train_station_stores_nan_open(df):
+#         mask = df.loc[:, 'DayOfWeek'] == 7.0
+#         dftrain2 = df[mask]
+#         dftrain3 = dftrain2.groupby('Store')['Open'].sum().to_frame().rename(columns={'Open': 'newopen'})
+#         train_station_stores = [i for i in dftrain3[(dftrain3.newopen > 3)].index]
+#
+#         if (pd.isnull(row['Open'])) & (row['Store'] in train_station_stores):
+#             return 1.0
+#         else:
+#             return row['Open']
+#
+#     df['Open'] = df.apply(train_station_stores_nan_open, axis=1)
+#
+#     print("Train station stores now open! Enjoy your sunday shopping!")
 
-        if (pd.isnull(row['Open'])) & (row['Store'] in train_station_stores):
-            return 1.0
-        else:
-            return row['Open']
-
-    df['Open'] = df.apply(train_station_stores_nan_open, axis=1)
-
-    print("Train station stores now open! Enjoy your sunday shopping!")
-
-# """Sets all Shops with isna('Open') to 0 on a German public holiday"""
-#    de_holidays = holidays.DE()
+    # """Sets all Shops with isna('Open') to 0 on a German public holiday"""
+    #de_holidays = holidays.DE()
 
     for i in range(len(df)):
         if (np.isnan(df['Open'][i])) & (df['Date'][i] in holidays.DE()):
@@ -83,21 +85,7 @@ def cleanData(df, ):
             pass
     print('Public Holidays updated')
 
-# Sets all Shops with isna('Open') to 0 based on stateholiday state
-    def open_stateholiday(row):
-        if pd.isnull(row['Open']) & (row['StateHoliday'] == 'a') & (row['StateHoliday'] == 'b') & (
-                row['StateHoliday'] == 'c'):
-            return 0.0
-        elif pd.isnull(row['Open']) & (row['StateHoliday'] == '0'):
-            return 1.0
-        else:
-            return row['Open']
-
-    df['Open'] = df.apply(open_stateholiday, axis=1)
-
-    print('Adjusted open status of shops according to state holidays')
-
-# take care of regional stateholiday
+    # take care of regional stateholiday
     for i in range(len(df)):
         if (pd.isnull(df['StateHoliday'][i])) & (df['Month'][i] == 1) & (df['Day'][i] == 6):
             if df['Year'][i] == 2013:
@@ -136,9 +124,9 @@ def cleanData(df, ):
         else:
             pass
 
-    print('finish regional stateholiday')
+    print('Finished regional stateholidays')
 
-# take care of remained stateholiday
+    # take care of remained stateholiday
     def remained_stateholiday(row):
         if (pd.isnull(row['StateHoliday'])):
             if pd.isnull(row['Open']):
@@ -156,6 +144,20 @@ def cleanData(df, ):
 
     df['StateHoliday'] = df.apply(remained_stateholiday, axis=1)
     print('finished remaining stateholidays')
+
+    # Sets all Shops with isna('Open') to 0 based on stateholiday state
+    def open_stateholiday(row):
+        if pd.isnull(row['Open']) & (row['StateHoliday'] == 'a') & (row['StateHoliday'] == 'b') & (
+                row['StateHoliday'] == 'c'):
+            return 0.0
+        elif pd.isnull(row['Open']) & (row['StateHoliday'] == '0'):
+            return 1.0
+        else:
+            return row['Open']
+
+    df['Open'] = df.apply(open_stateholiday, axis=1)
+
+    print('Adjusted open status of shops according to state holidays')
 
 # fill empty 'Customers' with average customer number when open=1.0, when open=0.0 customer=0.0
     df_mean_customers = df.loc[:, 'Customers'].mean()
@@ -175,7 +177,7 @@ def cleanData(df, ):
 
 
     # Fills empty 'Sales'-Cells in train with average if there have been non 0 customers in the shop
-    mean_sales = train['Sales'].mean()
+    mean_sales = df['Sales'].mean()
 
     def helper_sales(row):
         if pd.isnull(row['Sales']) & (row['Customers'] > 0):
@@ -192,41 +194,32 @@ def cleanData(df, ):
     df = pd.concat([df, NewStateHoliday], axis=1)
     print('State Holidays Encoded')
 
-
-
     #Gets dummies for 'PromoInterval' into three columns and concat them to the table
     PromoInterval = pd.get_dummies(df['PromoInterval'])
     df = pd.concat([df, PromoInterval], axis=1)
     print('PromoIntervals encoded')
 
-store = promoInterval(store)
 
-def fillEmptyDistances(row):
-    """FIlling empty distances with mean"""
-    if pd.isnull(row['CompetitionDistance']):
-        return store['CompetitionDistance'].mean()
-    else:
-        return row['CompetitionDistance']
+    def fillEmptyDistances(row):
+        """Filling empty distances with mean"""
+        if pd.isnull(row['CompetitionDistance']):
+            return df['CompetitionDistance'].mean()
+        else:
+            return row['CompetitionDistance']
 
-store['CompetitionDistance'] = store.apply(fillEmptyDistances, axis=1)
+    df['CompetitionDistance'] = df.apply(fillEmptyDistances, axis=1)
 
-def encodeStoreType(store):
-    """Gets dummies for 'StoreType' into four columns and concat them to the table"""
-    NewStoreType = pd.get_dummies(store['StoreType'])
+    #Encoding store types
+    NewStoreType = pd.get_dummies(df['StoreType'])
     NewStoreType.rename(columns={'a': 'StoreType a', 'b': 'StoreType b', 'c': 'StoreType c', 'd': 'StoreType d'},
                         inplace=True)
-    store = pd.concat([store, NewStoreType], axis=1)
+    store = pd.concat([df, NewStoreType], axis=1)
     print('Store Type Encoded')
-    return store
 
-store = encodeStoreType(store)
 
-def encodeAssortment(store):
-    """Gets dummies for 'Assortment' into three columns and concat them to the table"""
+    #Gets dummies for 'Assortment' into three columns and concat them to the table
     NewAssortment = pd.get_dummies(store['Assortment'])
     NewAssortment.rename(columns={'a': 'Basic Assort', 'b': 'Extra Assort', 'c': 'Extended Assort'}, inplace=True)
-    store = pd.concat([store, NewAssortment], axis=1)
+    store = pd.concat([df, NewAssortment], axis=1)
     print('Assortment Type Encoded')
-    return store
-
-store = encodeAssortment(store)
+    print('---Cleaning completed---')
